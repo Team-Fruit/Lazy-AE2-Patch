916857859ee26a0e873e885cc921711435944696
sjcl <bebe@sjcl.me>
2023-06-05 02:59:30 +0900

    Remove unused logger

--- src/main/java/net/teamfruit/lazyae2patch/Reference.java
+++ src/main/java/net/teamfruit/lazyae2patch/Reference.java
@@ -9,5 +9,5 @@ public class Reference {
     public static final String NAME = "Lazy AE2 Patch";
     public static final String VERSION = "1.0.0";
 
-    public static Logger logger = LogManager.getLogger(MODID);
+//    public static Logger logger = LogManager.getLogger(MODID);
 }
