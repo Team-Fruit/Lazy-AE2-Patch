a75494423a142de01f6942a2cb3c7167afc62dca
sjcl <bebe@sjcl.me>
2023-06-04 18:47:55 +0900

    Setup

---
+++ src/main/java/net/teamfruit/lazyae2patch/LazyAE2Patch.java
@@ -0,0 +1,7 @@
+package net.teamfruit.lazyae2patch;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid =  Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class LazyAE2Patch {
+}
---
+++ src/main/java/net/teamfruit/lazyae2patch/Reference.java
@@ -0,0 +1,13 @@
+package net.teamfruit.lazyae2patch;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Reference {
+
+    public static final String MODID = "lazyae2patch";
+    public static final String NAME = "Lazy-AE2 Patch";
+    public static final String VERSION = "1.0";
+
+    public static Logger logger = LogManager.getLogger(MODID);
+}
