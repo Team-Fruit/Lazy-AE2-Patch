36185c5e4e5720b2562de161083620dcf1f3589b
sjcl <bebe@sjcl.me>
2023-06-05 02:58:57 +0900

    Update versioning

--- src/main/java/net/teamfruit/lazyae2patch/Reference.java
+++ src/main/java/net/teamfruit/lazyae2patch/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
     public static final String MODID = "lazyae2patch";
     public static final String NAME = "Lazy AE2 Patch";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     public static Logger logger = LogManager.getLogger(MODID);
 }
