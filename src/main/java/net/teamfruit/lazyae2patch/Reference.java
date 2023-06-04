3dd641e86f37f46005f92569a0f539ac0b61315e
sjcl <bebe@sjcl.me>
2023-06-05 02:45:30 +0900

    Update mod name

--- src/main/java/net/teamfruit/lazyae2patch/Reference.java
+++ src/main/java/net/teamfruit/lazyae2patch/Reference.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
 
     public static final String MODID = "lazyae2patch";
-    public static final String NAME = "Lazy-AE2 Patch";
+    public static final String NAME = "Lazy AE2 Patch";
     public static final String VERSION = "1.0";
 
     public static Logger logger = LogManager.getLogger(MODID);
