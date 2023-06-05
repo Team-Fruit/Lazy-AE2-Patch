db563930bfcf3b1b9b50ab6254752e9bffa92ae6
sjcl <bebe@sjcl.me>
2023-06-05 20:39:33 +0900

    Refactor

--- src/main/java/net/teamfruit/lazyae2patch/mixins/MixinContainerTile.java
+++ src/main/java/net/teamfruit/lazyae2patch/mixins/MixinContainerTile.java
@@ -3,12 +3,12 @@ package net.teamfruit.lazyae2patch.mixins;
 import io.github.phantamanta44.libnine.gui.L9Container;
 import io.github.phantamanta44.threng.inventory.base.ContainerTile;
 import net.minecraft.tileentity.TileEntity;
-import net.teamfruit.lazyae2patch.IMixinContainerLevelMaintainer;
+import net.teamfruit.lazyae2patch.IMixinContainerTile;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ContainerTile.class)
-public abstract class MixinContainerTile<T extends TileEntity> extends L9Container implements IMixinContainerLevelMaintainer<T> {
+public abstract class MixinContainerTile<T extends TileEntity> extends L9Container implements IMixinContainerTile<T> {
 
     @Shadow(remap = false)
     protected T tile;
